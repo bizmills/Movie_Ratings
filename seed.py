00494367d1d4e67da1fa6a1026880279fdce5478
Hackbright Student <info@hackbrightacademy.com>
2014-06-30 17:45:58 -0700

    load_users and load_movies functions complete

--- seed.py
+++ seed.py
@@ -1,8 +1,6 @@
 import model
 import csv
-
-# from sqlalchemy.orm import sessionmaker
-
+import datetime
 
 def load_users(session):
     # use u.user
@@ -17,21 +15,36 @@ def load_users(session):
             zipcode = line[4]
             user = model.User(age=user_age, zipcode=zipcode)
             session.add(user)
-    print session
 
-    
 
 def load_movies(session):
     # use u.item
-    pass
+    with open("seed_data/u.item") as f:
+        reader = csv.reader(f, delimiter = '|')
+        for line in reader:
+            if line[2]:
+                release_date = datetime.datetime.strptime(line[2], "%d-%b-%Y")
+            else:
+                release_date = None 
+            title = line[1]
+            title = title.decode("latin-1")
+            movie = model.Movie(name=title, released_at=release_date, imdb_url=line[4])
+            session.add(movie)
+           
 
 def load_ratings(session):
     # use u.data
+    # with open("seed_data/u.data")
     pass
+
+# def release_date(str_date): 
+#     date_obj = datetime.datetime.strptime(str_date, "%d-%b-%Y")
+#     return date_obj
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
     load_users(session)
+    load_movies(session)
     session.commit()
 
 if __name__ == "__main__":
