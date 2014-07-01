777d60256d6bb8c82d8c93bb5ee04d3866138157
Hackbright Student <info@hackbrightacademy.com>
2014-06-30 17:03:34 -0700

    initial commit

--- model.py
+++ model.py
@@ -1,13 +1,54 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import sessionmaker 
+from datetime import datetime
+
+ENGINE = None
+Session = None
 
 Base = declarative_base()
 
 ### Class declarations go here
+class User(Base):
+    __tablename__ = "users"
 
+    id = Column(Integer, primary_key = True)
+    email = Column(String(64), nullable = True)
+    password = Column(String(64), nullable = True)
+    age = Column(Integer, nullable=True)
+    zipcode = Column(String(15), nullable=True)
+
+class Movie(Base):
+    __tablename__= "movies"
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(100), nullable = True)
+    released_at = Column(DateTime, nullable = True)
+    imdb_url = Column(String(100), nullable = True)
+
+class Rating(Base):
+    __tablename__ = "ratings"
+
+    id = Column(Integer, primary_key = True)
+    movie_id = Column(Integer, nullable = True)
+    user_id = Column(Integer, nullable = True)
+    rating = Column(Integer, nullable = True)
 
 ### End class declarations
+def connect():
+    global ENGINE 
+    global Session
+
+    ENGINE = create_engine("sqlite:///ratings.db", echo=False)
+    Session = sessionmaker(bind=ENGINE)
+
+    return Session()
+
+# NON FUNCTIONING STRING PARSING FOR RELEASED_AT
+# def datetime(str_date): 
+#     date_obj = datetime.datetime.strptime(str_date, "%d-%b-%Y")
+#     return date_obj
 
 def main():
     """In case we need this for something"""
--- seed.py
+++ seed.py
@@ -1,9 +1,25 @@
 import model
 import csv
 
+# from sqlalchemy.orm import sessionmaker
+
+
 def load_users(session):
     # use u.user
-    pass
+    with open("seed_data/u.user") as f:
+        reader = csv.reader(f, delimiter = '|')
+        for line in reader:
+            # debug counter
+            # i = 1
+            # if i % 10 == 0:
+            #     print i
+            user_age = line[1]
+            zipcode = line[4]
+            user = model.User(age=user_age, zipcode=zipcode)
+            session.add(user)
+    print session
+
+    
 
 def load_movies(session):
     # use u.item
@@ -15,7 +31,8 @@ def load_ratings(session):
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
-    pass
+    load_users(session)
+    session.commit()
 
 if __name__ == "__main__":
     s= model.connect()
