d7beb131255ec50735577cac8e72297227a4564b
Hackbright Student <info@hackbrightacademy.com>
2014-07-02 18:07:34 -0700

    Login page broken

--- judgement.py
+++ judgement.py
@@ -17,7 +17,7 @@ def show_signup():
 @app.route("/signup", methods=["POST"])
 def process_sign_up():
     d = request.form
-    print "_____________%r" % d
+    # print "_____________%r" % d
     # 1) create a User object with form data
     new_user = model.User(email=d['email'], password=d['password'])
     # 2) add object to db
@@ -26,8 +26,16 @@ def process_sign_up():
     model.db_session.commit()
     return render_template("signup.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET"])
+def show_login():
+    return render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
 def login():
+    user = model.db_session.query(model.User).filter_by(email=request.form['email']).filter_by(password=request.form['password']).one()
+    print user
+
     return render_template("login.html")
 
 if __name__== "__main__":
